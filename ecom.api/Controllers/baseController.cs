c542a454b5ff99830d0531b55dc88554fee95f1b
mohamed <mohamedtarek12633@gmail.com>
2026-02-15 07:00:33 +0200

    categorycontroller getall

---
+++ ecom.api/Controllers/baseController.cs
@@ -0,0 +1,17 @@
+﻿using ecom.core.interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecom.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class baseController : ControllerBase
+    {
+        protected readonly iunitofwork work;
+        public baseController(iunitofwork work)
+        {
+            this.work = work; 
+        }
+    }
+}
---
+++ ecom.api/Controllers/categoriesController.cs
@@ -0,0 +1,31 @@
+﻿using ecom.core.interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecom.api.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class categoriesController : baseController
+    {
+        public categoriesController(iunitofwork work) : base(work)
+        {
+        }
+        [HttpGet("get-all")]
+        public async Task<IActionResult> get()
+        {
+            try
+            {
+                var catregory = await work.categoryrepository.getallasync();
+                if (catregory is null) 
+                    return BadRequest();
+                return Ok(catregory);
+                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
